0195c9ee4947bb903b2d9134aa29fd73bfee1680
= <luiz.pinheiro@fcamara.com.br>
2019-09-17 12:14:33 -0300

    feat(): Adding emotion styled.

--- src/views/home/home.view.js
+++ src/views/home/home.view.js
@@ -1,18 +1,43 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styled from '@emotion/styled';
 
 const Home = ({ value, click }) => (
-  <div>
-    <h1>
+  <>
+    <Text>
       Hi, increment the value:
-      {' '}
-      {value}
-      <button type='button' onClick={() => click(1)}>
+      <Number>{value}</Number>
+      <Button type='button' onClick={() => click(1)}>
         increment
-      </button>
-    </h1>
-  </div>
+      </Button>
+    </Text>
+  </>
 );
+
+const Number = styled.span`
+  font-size: 50px;
+  font-weight: bold;
+  display: block;
+`;
+
+const Text = styled.h1`
+  font-size: 40px;
+  width: 100%;
+  display: block;
+  text-align: center;
+  padding-top: 15%;
+`;
+
+const Button = styled.button`
+  background: #000;
+  color: #fff;
+  width: 10%;
+  border: none;
+  border-radius: 5px;
+  padding: 10px;
+  text-align: center;
+  cursor: pointer;
+`;
 
 Home.propTypes = {
   value: PropTypes.number,
